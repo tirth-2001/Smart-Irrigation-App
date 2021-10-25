35df5ace8919b19cb84266f20a60ee76ecbbaf82
tirthpatel-ansorbit <tirth.patel@ansorbit.com>
2021-10-25 17:48:29 +0530

    Centralized storage, auth, firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,17 @@
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
+import storage from '@react-native-firebase/storage'
 
-// const firestore = app.firestore()
-// export const database = {
-//   folders: firestore.collection('folders'),
-//   files: firestore.collection('files'),
-//   formatDoc: doc => {
-//     return {id: doc.id, ...doc.data()}
-//   },
-//   getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
-// }
-// export const storage = app.storage()
+const firestore = firestore()
+
+export const database = {
+  farmers: firestore.collection('farmers'),
+  admins: firestore.collection('irrigation_admins'),
+  formatDoc: doc => {
+    return {id: doc.id, ...doc.data()}
+  },
+}
+
+export const storageRef = storage().ref('/irrigation_app')
+
 export const auth = auth()
